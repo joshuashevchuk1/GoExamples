663eeb538bb449aea29c3fc4b27b9ae81a3c3ad6
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-07 12:43:34 -0800

    while go is great for async practices, lets try concurrency in python

--- practice/practice1.go
+++ practice/practice1.go
@@ -1,1 +1,18 @@
 package practice
+
+// ### Example #1: Concurrent File Download
+//
+// Implement a function that, given an array of URLs and an existing download function,
+// downloads all the data from the urls in parallel, merges the results into a single dictionary of {url:data}
+// and then returns the dictionary.
+//
+// Result: they are separate JSON payloads w/o overlapping keys. i.e., the download result of each URL is a number
+// and we want to return the total sum at the end.
+
+type ActionableUrl func() error
+
+func GetUrl() {
+	mu.Unlock()
+	defer mu.Lock()
+	// act on the url here and get the json result
+}
