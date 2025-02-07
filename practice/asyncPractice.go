e7d0fc70bc4bfeb817723fc5c0ec23ce7441fb19
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-02-07 12:01:36 -0800

    global variable update of n

--- practice/asyncPractice.go
+++ practice/asyncPractice.go
@@ -1,26 +1,29 @@
 package practice
 
 import (
-	"errors"
 	"fmt"
 	"sync"
 )
 
 type Action func() error
 
-var n = 0
+var (
+	n  = 0
+	mu sync.Mutex // Mutex for safe concurrent updates
+)
 
-func Actionable(str string, n int) *int {
+func Actionable(str string) {
+	mu.Lock()
+	defer mu.Unlock()
 	fmt.Println(str)
-	n = n + 1
-	return &n
+	n++ // Modify the global variable safely
 }
 
-func ActionableAtN(n int) error {
-	actions := make([]Action, n)
+func ActionableAtN(count int) error {
+	actions := make([]Action, count)
 	for i := range actions {
 		actions[i] = func() error {
-			n = *Actionable("Hello World", n)
+			Actionable("Hello World") // Use global `n` safely
 			return nil
 		}
 	}
@@ -33,28 +36,30 @@ func ActionableAtN(n int) error {
 }
 
 func GetN() int {
+	mu.Lock()
+	defer mu.Unlock()
 	return n
 }
 
 func Parallel(actions ...Action) error {
 	var wg sync.WaitGroup
-	wg.Add(len(actions))
+	errChan := make(chan error, len(actions))
 
 	for _, action := range actions {
-		go func(a Action) { // Create a copy of action
-			defer func() {
-				if r := recover(); r != nil {
-					_ = errors.New("panic recovered")
-				}
-				wg.Done() // Ensure wg.Done() is always called
-			}()
-			err := a()
-			if err != nil {
-				return
-			} // Actually execute the action
+		wg.Add(1)
+		go func(a Action) {
+			defer wg.Done()
+			if err := a(); err != nil {
+				errChan <- err
+			}
 		}(action)
 	}
 
 	wg.Wait()
+	close(errChan)
+
+	for err := range errChan {
+		return err // Return the first error encountered
+	}
 	return nil
 }
